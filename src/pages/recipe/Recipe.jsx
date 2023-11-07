d2cd40d5a818b565ba40bd44a990b31d5feef0f6
William East <wjbetech@gmail.com>
2023-11-07 17:09:47 +0900

    Finished basic setup, read README

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,14 +1,16 @@
-import "./Recipe.css"
+import "./Recipe.css";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { useTheme } from "../../hooks/useTheme";
 import { myProjectFirestore } from "../../firebase/config";
 
 export default function Recipe() {
+  // call our params
+  const { id } = useParams();
 
   // call our themes
-  const { mode } = useTheme()
+  const { mode } = useTheme();
 
   // set states
   const [data, setData] = useState(null);
@@ -21,17 +23,28 @@ export default function Recipe() {
   // get our data via useEffect
   useEffect(() => {
     setIsPending(true);
-    myProjectFirestore.collection("recipes").doc(recipe).get()
-    .then((document) => {
-      if (document.exists) {
-        setIsPending(false);
-        setData(document.data())
-      } else {
-        setIsPending(false);
-        setError("Could not find recipe!")
-      }
-    }
-  )}, [recipe])
+
+    const unsub = myProjectFirestore
+      .collection("recipes")
+      .doc(recipe)
+      .onSnapshot((document) => {
+        if (document.exists) {
+          setIsPending(false);
+          setData(document.data());
+        } else {
+          setIsPending(false);
+          setError("Could not find recipe!");
+        }
+      });
+
+    return () => unsub();
+  }, [recipe]);
+
+  const handleClick = () => {
+    myProjectFirestore.collection("recipes").doc(recipe).update({
+      title: "Stew de la Veg",
+    });
+  };
 
   return (
     <div className={`recipe recipe-${mode}`}>
@@ -41,16 +54,23 @@ export default function Recipe() {
         <>
           <h1 className="recipe-name">{data.title}</h1>
           <hr className="recipe-divider" />
-          <ul className="recipe-ingredients">{data.ingredients.map(i => (
-            <li key={i} className="ingredient-item">{i}</li>
-          ))}</ul>
+          <ul className="recipe-ingredients">
+            {data.ingredients.map((i) => (
+              <li key={i} className="ingredient-item">
+                {i}
+              </li>
+            ))}
+          </ul>
           <p className="recipe-time">Cooking time: {data.cookingTime}</p>
           <p className="recipe-method">{data.method}</p>
+          <button className="recipe-button" onClick={handleClick}>
+            Edit Recipe
+          </button>
           <Link to="/">
             <button>Back to Recipes</button>
           </Link>
-      </>
+        </>
       )}
     </div>
-  )
+  );
 }
